2d3c3f0fef0045605bc94880482329d36ce69ffb
ahmed <aelshabrawy60@gmail.com>
2024-10-17 22:50:35 +0300

    intiate

---
+++ news/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { BrowserRouter, Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css'
+import Home from './Pages/Home/Home';
+import News from './Pages/News/News';
+import NewsOpen from './Pages/NewsOpen/NewsOpen';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/news" element={<News />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
